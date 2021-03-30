917690f67c49f2320bb3c62d1faf5bb557cd9832
BuildTools <unconfigured@null.spigotmc.org>
2021-03-30 14:06:11 -0600

    Changing name of the server

--- mac-run-server.sh
+++ mac-run-server.sh
@@ -3,7 +3,7 @@
 docker run --name=valheim -d \
 -p 2456:2456/udp -p 2457:2457/udp -p 2458:2458/udp \
 -v /Users/dylanschlager/valheim/valheim-server/data:/home/steam/valheim-data \
---env VALHEIM_SERVER_NAME="Dylan's Server" \
---env VALHEIM_WORLD_NAME="Dedicated" \
+--env VALHEIM_SERVER_NAME="Schlags Party Time Server" \
+--env VALHEIM_WORLD_NAME="Oridiath" \
 --env VALHEIM_PASSWORD="Zenica" \
-schlags/valheim-server
+schlags/valheim-server-updated
